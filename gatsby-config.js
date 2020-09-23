98405866a11178cc2db2295fef019c78cdadb7e5
Jack O'Hara <jack.ohara@sorted.com>
2020-09-23 07:30:55 +0100

    Updated all the metadata to be harriers stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Clayton-Le-Moors Harriers`,
+    description: `Welcome to Clayton-Le-Moors Harriers! Find all the latest news and info and get in touch if you're interested in joining!`,
+    author: `Jack O'Hara`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
