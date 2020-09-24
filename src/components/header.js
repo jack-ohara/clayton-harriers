ec45b1c8e41918445c4f60446cfb8cae8242a509
Jack O'Hara <jack.ohara@sorted.com>
2020-09-24 21:22:54 +0100

    Converted all inline styles to styled components and started to customise some of the content

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,35 @@
-import { Link } from "gatsby"
+import { Link, useStaticQuery } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Img from "gatsby-image"
+import styled from "styled-components"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const StyledHeader = styled.header`
+  margin-bottoml: 1.45rem;
+  padding: 1rem 0.5rem;
+`
+
+const Header = () => {
+  const logoData = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "harriers-logo-transparent.png" }) {
+        childImageSharp {
+          fixed(width: 65, height: 65) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <StyledHeader>
+      <Link to="/">
+        <Img fixed={logoData.file.childImageSharp.fixed} />
+      </Link>
+    </StyledHeader>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,23 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
+import styled from "styled-components"
 import "./layout.css"
+
+const Container = styled.div`
+  min-height: 100vh;
+`
+
+const ContentWrapper = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45rem;
+`
+
+const Footer = styled.footer`
+  margin-top: 2rem;
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,25 +31,13 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <Container>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <ContentWrapper>
         <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+        <Footer>© {new Date().getFullYear()} Clayton-Le-Moors Harriers</Footer>
+      </ContentWrapper>
+    </Container>
   )
 }
 
