9dc7e9605063a2479c33b0f5042e00794dec2a32
Jack O'Hara <jack.ohara@sorted.com>
2020-09-26 09:28:52 +0100

    Added a square banner and added it as the site's icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/harriers-logo-transparent.png`, // This path is relative to the root of the site.
+        icon: `src/images/whole-banner-square.svg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-netlify-cms`,
