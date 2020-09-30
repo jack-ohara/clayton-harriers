baee4c23dd1b3e6645abfe4b723b45ce80328943
Jack O'Hara <jack.ohara@sorted.com>
2020-09-30 21:24:53 +0100

    Added the burger menu components and added it to the header

---
+++ src/components/burgerMenu.js
@@ -0,0 +1,107 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+const StyledMenu = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: #effffa;
+  transform: ${({ open }) => (open ? "translateY(0)" : "translateY(-100%)")};
+  text-align: left;
+  padding: 3rem 2rem 0 2rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  transition: transform 0.3s ease-in-out;
+  z-index: 5;
+
+  @media (max-width: 576px) {
+    width: 100%;
+  }
+
+  a {
+    font-size: 2rem;
+    padding: 1.5rem 0;
+    font-weight: bold;
+    letter-spacing: 0.5rem;
+    color: #0d0c1d;
+    text-decoration: none;
+    transition: color 0.3s linear;
+    background-image: none;
+
+    @media (max-width: 576px) {
+      font-size: 1.5rem;
+      text-align: center;
+    }
+
+    &:hover {
+      color: #343078;
+    }
+  }
+`
+
+export const Menu = ({ open }) => {
+  return (
+    <StyledMenu open={open}>
+      <Link to="/">Home</Link>
+      <Link to="/news">News &amp; Info</Link>
+      <Link to="/training">Training</Link>
+      <Link to="/join-us">Join Us</Link>
+    </StyledMenu>
+  )
+}
+
+const StyledBurger = styled.button`
+  position: absolute;
+  top: 5%;
+  right: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 10;
+
+  &:focus {
+    outline: none;
+  }
+
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background: #0d0c1d;
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+
+    :first-child {
+      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
+    }
+
+    :nth-child(2) {
+      opacity: ${({ open }) => (open ? "0" : "1")};
+      transform: ${({ open }) => (open ? "translateX(20px)" : "translateX(0)")};
+    }
+
+    :nth-child(3) {
+      transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
+    }
+  }
+`
+
+export const Burger = ({ open, setOpen }) => {
+  return (
+    <StyledBurger open={open} onClick={() => setOpen(!open)}>
+      <div />
+      <div />
+      <div />
+    </StyledBurger>
+  )
+}
--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import { Link, useStaticQuery, graphql } from "gatsby"
 import React from "react"
 import Img from "gatsby-image"
 import styled from "styled-components"
+import { Burger, Menu } from "./burgerMenu"
 
 const StyledHeader = styled.header`
   padding: 1rem 0.5rem;
@@ -24,11 +25,18 @@ const Header = () => {
     }
   `)
 
+  const [open, setOpen] = React.useState(false)
+  const node = React.useRef()
+
   return (
     <StyledHeader>
       <LogoLink to="/">
         <Img fixed={logoData.file.childImageSharp.fixed} />
       </LogoLink>
+      <div ref={node}>
+        <Burger open={open} setOpen={setOpen} />
+        <Menu open={open} setOpen={setOpen} />
+      </div>
     </StyledHeader>
   )
 }
