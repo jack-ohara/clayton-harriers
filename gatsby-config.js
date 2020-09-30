449e721f7bd6a114af4e9181f3b9f1b0e89938e3
Jack O'Hara <jack.ohara@sorted.com>
2020-09-30 13:56:39 +0100

    With the preview template removed it builds fine

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
-        modulePath: `${__dirname}/src/cms/index.tsx`,
+        modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
     {
--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,7 @@
 import CMS from "netlify-cms-app"
 import cloudinary from "netlify-cms-media-library-cloudinary"
 
-import NewsPostPreview from "./preview-templates/NewsPostPreview"
+//import NewsPostPreview from "./preview-templates/NewsPostPreview"
 
 CMS.registerMediaLibrary(cloudinary)
 
