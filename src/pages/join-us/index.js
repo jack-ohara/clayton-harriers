ba5276b98f5cffc009c4b96239a315974424a9f5
Jack O'Hara <jack@jackohara.io>
2021-05-12 21:24:15 +0100

    Added join-us page support

--- src/pages/join-us/index.js
+++ src/pages/join-us/index.js
@@ -1,15 +1,27 @@
 import React from "react"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import { graphql } from "gatsby"
 
-const JoinUsPage = () => {
+const JoinUsPage = ({ data }) => {
+  console.log(data)
   return (
     <Layout>
       <SEO title="Join Us" />
 
       <h1>Join Us</h1>
+
+      <div dangerouslySetInnerHTML={{ __html: data.wpPage.content }} />
     </Layout>
   )
 }
 
 export default JoinUsPage
+
+export const pageQuery = graphql`
+  query JoinUsPageQuery {
+    wpPage(slug: { eq: "join" }) {
+      content
+    }
+  }
+`
